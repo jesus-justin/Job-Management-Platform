0c5eee3a57262e2cabe4a8b0b33da567c37196c1
jesus-justin <mjesusjustin@gmail.com>
2024-11-19 14:14:25 +0800

    Update submit_application.php

--- submit_application.php
+++ submit_application.php
@@ -4,19 +4,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = htmlspecialchars($_POST['email']);
     $jobTitle = htmlspecialchars($_POST['jobTitle']);
     $reason = htmlspecialchars($_POST['reason']);
+    
+    include 'submit_application.html';
+} else {
+    header('Location: job_listings.php');
+    exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Application Successful</title>
-</head>
-<body>
-    <h1>Application Successful!</h1>
-    <p>Thank you, <?php echo $name; ?>, for applying for the <strong><?php echo $jobTitle; ?></strong> position.</p>
-    <p>We will contact you at <?php echo $email; ?> regarding your application status.</p>
-    <p><a href="job_listings.php">Back to Job Listings</a></p>
-</body>
-</html>
